6d54f0e7895eb4564f0303464cf462d830bf4279
unknown <camposmarbella898029@ymail.com>
2021-03-09 13:10:56 -0600

    added using statement to Owner model

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -17,5 +18,8 @@ namespace PawMates.Models
         [ForeignKey("SlackUserID")]
         public string SlackUserId { get; set; }
 
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
