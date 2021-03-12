9d4c924400079bf917a7796c6dac3f7be2edba9b
unknown <camposmarbella898029@ymail.com>
2021-03-12 14:52:05 -0600

    added signalr.js, chat.js, ChatHub.cs, & Message view. Added service to ConfigureServices methos and an endpoint to the Configure method in the StartUp class

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
 
-
-namespace SignalRChat.Hubs
+namespace PawMates.Hubs
 {
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message)
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
 }
